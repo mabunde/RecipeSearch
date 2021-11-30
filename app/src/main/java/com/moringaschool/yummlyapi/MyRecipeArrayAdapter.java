1be2a9b7460b1a0b7dfa42c6c4e0cff8fa9f05f3
mabunde <solomonsitati39@gmail.com>
2021-11-30 08:17:19 +0300

    Create my array adapter

--- app/src/main/java/com/moringaschool/yummlyapi/MyRecipeArrayAdapter.java
+++ app/src/main/java/com/moringaschool/yummlyapi/MyRecipeArrayAdapter.java
@@ -1,4 +1,29 @@
 package com.moringaschool.yummlyapi;
 
-public class MyRecipeArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyRecipeArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mRecipe;
+    private String[] mIngredients;
+
+    public MyRecipeArrayAdapter(Context mContext, int resource, String[] mRecipe, String[] mIngredients) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mRecipe = mRecipe;
+        this.mIngredients = mIngredients;
+    }
+    @Override
+    public Object getItem(int position) {
+        String restaurant = mRecipe[position];
+        String cuisine = mIngredients[position];
+        return String.format("%s \nServes great: %s", recipe, ingredients);
+    }
+
+    @Override
+    public int getCount() {
+        return mRecipe.length;
+    }
+
 }
