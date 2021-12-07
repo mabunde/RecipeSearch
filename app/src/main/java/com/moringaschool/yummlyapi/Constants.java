e07b06b7109801b3d84a95eb2a9b931320a77962
mabunde <solomonsitati39@gmail.com>
2021-12-07 04:30:02 +0300

    Add constant class

---
+++ app/src/main/java/com/moringaschool/yummlyapi/Constants.java
@@ -0,0 +1,7 @@
+package com.moringaschool.yummlyapi;
+
+public class Constants {
+    public static final String YUMMLY_BASE_URL = "http://api.yummly.com/v1/";
+    public static final String YUMMLY_API_KEY = BuildConfig.YUMMLY_API_KEY;
+
+}
