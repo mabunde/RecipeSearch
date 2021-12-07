2cdfc71eec892009aeac25c97e07cfd580606058
mabunde <solomonsitati39@gmail.com>
2021-12-07 21:57:50 +0300

    Change the base URL for my API

--- app/src/main/java/com/moringaschool/yummlyapi/Constants.java
+++ app/src/main/java/com/moringaschool/yummlyapi/Constants.java
@@ -1,7 +1,7 @@
 package com.moringaschool.yummlyapi;
 
 public class Constants {
-    public static final String SPOONACULAR_BASE_URL = "http://api.yummly.com/v1/";
+    public static final String SPOONACULAR_BASE_URL = "https://api.spoonacular.com/";
     public static final String SPOONACULAR_API_KEY = BuildConfig.SPOONACULAR_API_KEY;
 
 }
