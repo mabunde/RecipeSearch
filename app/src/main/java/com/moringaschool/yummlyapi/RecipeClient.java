72e0bbd6c9f9d575fb2608ab5059a976b1144b6c
mabunde <solomonsitati39@gmail.com>
2021-12-07 22:36:26 +0300

    Create recipeClient

---
+++ app/src/main/java/com/moringaschool/yummlyapi/RecipeClient.java
@@ -0,0 +1,17 @@
+package com.moringaschool.yummlyapi;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RecipeClient {
+    private static Retrofit retrofit=null;
+
+    public static SpoonacularApi getClient(){
+        if(retrofit==null){
+            retrofit=new Retrofit.Builder()
+                    .baseUrl(Constants.SPOONACULAR_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit.create(SpoonacularApi.class );
+    }
+}
--- app/src/main/java/com/moringaschool/yummlyapi/SpoonacularApi.java
+++ app/src/main/java/com/moringaschool/yummlyapi/SpoonacularApi.java
@@ -4,7 +4,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
-public interface spoonacularApi {
+public interface SpoonacularApi {
     @GET("recipes/complexSearch")
     Call<SpoonacularRecipeSearchResponse> getSearchRecipes(
             @Query("apiKey") String myAPIKey
