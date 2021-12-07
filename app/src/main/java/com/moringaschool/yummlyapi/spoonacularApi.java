5cb8a0431c0c593e38d1f711945c8a2901c088a2
mabunde <solomonsitati39@gmail.com>
2021-12-07 22:22:55 +0300

    create spoonacular API interface

---
+++ app/src/main/java/com/moringaschool/yummlyapi/spoonacularApi.java
@@ -0,0 +1,12 @@
+package com.moringaschool.yummlyapi;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface spoonacularApi {
+    @GET("recipes/complexSearch")
+    Call<SpoonacularRecipeSearchResponse> getSearchRecipes(
+            @Query("apiKey") String myAPIKey
+            );
+}
