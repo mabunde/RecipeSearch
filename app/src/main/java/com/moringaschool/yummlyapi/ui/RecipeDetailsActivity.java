1d2a9c143b23b912e0c0d0f6e57f0902c17cb1ab
mabunde <solomonsitati39@gmail.com>
2021-12-08 14:48:55 +0300

    Add recipe detail fragment

---
+++ app/src/main/java/com/moringaschool/yummlyapi/ui/RecipeDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.moringaschool.yummlyapi.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.yummlyapi.R;
+
+public class RecipeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_details);
+
+    }
+}
